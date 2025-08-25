d0e0a9054ccd431ca39e44d0f29e3fd7b8c99f1f
Alex Alexeev <21alexeev99@gmail.com>
2025-08-26 01:17:51 +0200

    BANK-2: Created an initial use screen with workflow

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,37 @@
 package com.mycompany.app;
 
+import com.mycompany.app.userconsole.InitialUserConsole;
+
 /**
  * Hello world!
  */
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        while (true) {
+            String state = "initial";
+            InitialUserConsole.printOperationMenu();
+            InitialUserConsole.printNewLine();
+            InitialUserConsole.printTerminationHint();
+            InitialUserConsole.printNewLine();
+
+            String nextOperation = InitialUserConsole.readUserInput().trim();
+
+            if ("initial".equals(state)) {
+                if ("1".equals(nextOperation)) {
+                    state = "createBankAccount";
+                } else if ("c".equals(nextOperation)) {
+                    InitialUserConsole.printTerminationNotification();
+                    break;
+                } else {
+                    InitialUserConsole.printInvalidInput();
+                    InitialUserConsole.printNewLine();
+                    InitialUserConsole.printNewLine();
+                    InitialUserConsole.printNewLine();
+                }
+            }
+
+            if ("createBankAccount".equals(state)) {}
+        }
     }
 }
