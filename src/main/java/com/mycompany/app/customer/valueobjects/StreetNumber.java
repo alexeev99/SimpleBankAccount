1d6776203a3974e9cbf8f56a25381b94207a9714
Alex Alexeev <21alexeev99@gmail.com>
2025-08-27 03:49:29 +0200

    BANK-2: Implemented StreetNumber with exception

---
+++ src/main/java/com/mycompany/app/customer/valueobjects/StreetNumber.java
@@ -0,0 +1,26 @@
+package com.mycompany.app.customer.valueobjects;
+
+import com.mycompany.app.customer.exceptions.StreetNumberNotIntegerException;
+
+public class StreetNumber {
+
+    private final String streetNumber;
+
+    public StreetNumber(String streetNumber) {
+        this.validateStreetNumber(streetNumber);
+        this.streetNumber = streetNumber;
+    }
+
+    private void validateStreetNumber(String streetNumber) {
+        try {
+            Integer.parseInt(streetNumber);
+        } catch (NumberFormatException e) {
+            throw new StreetNumberNotIntegerException(streetNumber);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.streetNumber;
+    }
+}
