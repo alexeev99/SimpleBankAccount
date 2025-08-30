04978099bc23e60fdc60e817bef4da8078693ecd
Alex Alexeev <21alexeev99@gmail.com>
2025-08-31 01:42:14 +0200

    BANK-3: Adjusted BankAccountCreationUseCase to use BankAccountCreationScreen class

--- src/main/java/com/mycompany/app/usecases/BankAccountCreationUseCase.java
+++ src/main/java/com/mycompany/app/usecases/BankAccountCreationUseCase.java
@@ -9,46 +9,99 @@ import com.mycompany.app.customer.valueobjects.NormalizedString;
 import com.mycompany.app.customer.valueobjects.PostalCode;
 import com.mycompany.app.customer.valueobjects.StreetNumber;
 import com.mycompany.app.customer.valueobjects.TaxId;
-import com.mycompany.app.userconsole.BankAccountCreationUserConsole;
-import com.mycompany.app.userconsole.valueobjects.CustomerUserConsoleInput;
-
+import com.mycompany.app.userconsole.screens.BankAccountCreationScreen;
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class BankAccountCreationUseCase {
 
+  public static final Scanner scanner = new Scanner(System.in);
+
   public static void execute() {
-    BankAccountCreationUserConsole.printWelcomeMessage();
-    BankAccountCreationUserConsole.printNewLine();
-    BankAccountCreationUserConsole.printCustomerInformationNecessary();
-    BankAccountCreationUserConsole.printNewLine();
-    Customer customer = BankAccountCreationUseCase.createCustomerFromUserConsole();
+    System.out.print(BankAccountCreationScreen.getWelcomeText());
+    System.out.println();
+    System.out.println();
+    Customer customer = readCustomerFromUserConsole2();
     BankId bankId = new BankId();
     SavingAccount savingAccount = new SavingAccount(bankId);
-    BankAccountCreationUserConsole.printNewLine();
-    BankAccountCreationUserConsole.printBankAccountSuccessfullyCreated();
-    BankAccountCreationUserConsole.printNewLine();
   }
 
-  private static Customer createCustomerFromUserConsole() {
-    CustomerUserConsoleInput customerInput = BankAccountCreationUserConsole.readCustomer();
+  private static Customer readCustomerFromUserConsole2() {
     CustomerId customerId = new CustomerId();
-    NormalizedString firstName = new NormalizedString(customerInput.firstName());
-    NormalizedString lastName = new NormalizedString(customerInput.lastName());
-    NormalizedString cityOfBirth = new NormalizedString(customerInput.cityOfBirth());
-    LocalDate birthDate = LocalDate.parse(customerInput.birthDate());
+    NormalizedString firstName = readCustomerFirstName();
+    NormalizedString lastName = readCustomerLastName();
+    NormalizedString cityOfBirth = readCustomerCityOfBirth();
+    LocalDate birthDate = readCustomerBirthDate();
+    TaxId taxId = readCustomerTaxId();
+    NormalizedString addressCity = readCustomerAddressCity();
+    PostalCode addressPostalCode = readCustomerAddressPostalCode();
+    NormalizedString addressStreetName = readCustomerAddressStreetName();
+    StreetNumber addressStreetNumber = readCustomerStreetNumber();
     Address address =
-        BankAccountCreationUseCase.createCustomerAddressFromUserConsole(customerInput);
-    TaxId taxId = new TaxId(customerInput.taxId());
-
+        new Address(addressCity, addressPostalCode, addressStreetNumber, addressStreetName);
     return new Customer(customerId, firstName, lastName, cityOfBirth, birthDate, address, taxId);
   }
 
-  private static Address createCustomerAddressFromUserConsole(
-      CustomerUserConsoleInput customerInput) {
-    NormalizedString city = new NormalizedString(customerInput.address().city());
-    PostalCode postalCode = new PostalCode(customerInput.address().postalCode());
-    StreetNumber streetNumber = new StreetNumber(customerInput.address().streetNumber());
-    NormalizedString streetName = new NormalizedString(customerInput.address().streetName());
-    return new Address(city, postalCode, streetNumber, streetName);
+  private static NormalizedString readCustomerFirstName() {
+    System.out.print(BankAccountCreationScreen.getFirstNameText());
+    String firstNameString = scanner.nextLine();
+    NormalizedString firstName = new NormalizedString(firstNameString);
+    return firstName;
+  }
+
+  private static NormalizedString readCustomerLastName() {
+    System.out.print(BankAccountCreationScreen.getLastNameText());
+    String lastNameString = scanner.nextLine();
+    NormalizedString lastName = new NormalizedString(lastNameString);
+    return lastName;
+  }
+
+  private static NormalizedString readCustomerCityOfBirth() {
+    System.out.print(BankAccountCreationScreen.getCityOfBirthText());
+    String cityOfBirthString = scanner.nextLine();
+    NormalizedString cityOfBirth = new NormalizedString(cityOfBirthString);
+    return cityOfBirth;
+  }
+
+  private static LocalDate readCustomerBirthDate() {
+    System.out.print(BankAccountCreationScreen.getBirthDateInput());
+    String birthDateString = scanner.nextLine();
+    LocalDate birthDate = LocalDate.parse(birthDateString);
+    return birthDate;
+  }
+
+  private static TaxId readCustomerTaxId() {
+    System.out.print(BankAccountCreationScreen.getTaxIdText());
+    String taxIdString = scanner.nextLine();
+    TaxId taxId = new TaxId(taxIdString);
+    return taxId;
+  }
+
+  private static NormalizedString readCustomerAddressCity() {
+    System.out.print(BankAccountCreationScreen.getAddressCityText());
+    String addressCityString = scanner.nextLine();
+    NormalizedString addressCity = new NormalizedString(addressCityString);
+    return addressCity;
+  }
+
+  private static PostalCode readCustomerAddressPostalCode() {
+    System.out.print(BankAccountCreationScreen.getAddressPostalCodeText());
+    String addressPostalCodeString = scanner.nextLine();
+    PostalCode addressPostalCode = new PostalCode(addressPostalCodeString);
+    return addressPostalCode;
+  }
+
+  private static NormalizedString readCustomerAddressStreetName() {
+    System.out.print(BankAccountCreationScreen.getAddressStreetNameText());
+    String addressStreetNameString = scanner.nextLine();
+    NormalizedString addressStreetName = new NormalizedString(addressStreetNameString);
+    return addressStreetName;
+  }
+
+  private static StreetNumber readCustomerStreetNumber() {
+    System.out.print(BankAccountCreationScreen.getAddressStreetNumberText());
+    String addressStreetNumberString = scanner.nextLine();
+    StreetNumber addressStreetName = new StreetNumber(addressStreetNumberString);
+    return addressStreetName;
   }
 }
