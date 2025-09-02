7965290d4137079537d13b6585d4e44de88dc23b
Alex Alexeev <21alexeev99@gmail.com>
2025-09-03 00:00:21 +0200

    BANK-4: Deleted unecessary BankAccount exceptions

--- src/main/java/com/mycompany/app/bankaccount/exceptions/BankAccountNegativeAmountEuroDepositException.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.app.bankaccount.exceptions;
-
-public class BankAccountNegativeAmountEuroDepositException extends RuntimeException {
-
-  public BankAccountNegativeAmountEuroDepositException(double amountEuro) {
-    super(String.format("A negative amount %.2f cannot be deposit.", amountEuro));
-  }
-}
