d395d3dc217dc2ac9a6f2a3637a1273695287316
Alex Alexeev <21alexeev99@gmail.com>
2025-09-03 01:17:05 +0200

    BANK-4: Finished implementation of the deposit use case

--- src/main/java/com/mycompany/app/usecases/BankAccountDepositUseCase.java
+++ src/main/java/com/mycompany/app/usecases/BankAccountDepositUseCase.java
@@ -1,6 +1,10 @@
 package com.mycompany.app.usecases;
 
+import com.mycompany.app.bankaccount.BankAccountManager;
+import com.mycompany.app.bankaccount.valueobjects.BankId;
+import com.mycompany.app.bankaccount.valueobjects.PositiveAmountEuro;
 import com.mycompany.app.userconsole.screens.depositscreens.InitialDepositScreen;
+import com.mycompany.app.userconsole.screens.depositscreens.SuccessfulDepositScreen;
 import java.util.Scanner;
 
 public class BankAccountDepositUseCase {
@@ -11,5 +15,34 @@ public class BankAccountDepositUseCase {
     System.out.print(InitialDepositScreen.getWelcomeText());
     System.out.println();
     System.out.println();
+
+    BankId bankId = BankAccountDepositUseCase.readBankId();
+    BankAccountManager bankAccountManager = BankAccountManager.getInstance();
+    bankAccountManager.find(bankId);
+    PositiveAmountEuro positiveAmountEuro = BankAccountDepositUseCase.readPositiveAmountEuro();
+    System.out.println();
+    System.out.println();
+
+    bankAccountManager.deposit(bankId, positiveAmountEuro);
+
+    SuccessfulDepositScreen.setSuccessfulTextFormatting(
+        positiveAmountEuro.toDouble(), bankId.toString());
+    System.out.print(SuccessfulDepositScreen.getSuccessfulText());
+    System.out.println();
+    System.out.println();
+  }
+
+  private static BankId readBankId() {
+    System.out.print(InitialDepositScreen.getBankIdText());
+    String bankIdString = scanner.nextLine();
+    BankId bankId = new BankId(bankIdString);
+    return bankId;
+  }
+
+  private static PositiveAmountEuro readPositiveAmountEuro() {
+    System.out.print(InitialDepositScreen.getMonetaryAmountEurText());
+    String positiveAmountEuroString = scanner.nextLine();
+    PositiveAmountEuro positiveAmountEuro = new PositiveAmountEuro(positiveAmountEuroString);
+    return positiveAmountEuro;
   }
 }
