a62a6890d9a2de6a0e75576b87db5915fe711393
Alex Alexeev <21alexeev99@gmail.com>
2025-09-05 21:44:33 +0200

    BANK-5: Implemented BankId test cases

---
+++ src/test/java/com/mycompany/app/unit/bankaccount/PositiveAmountEuroTest.java
@@ -0,0 +1,12 @@
+/*
+Topics to document:
+- How to write test cases
+    - Normal test case
+    - With static mocking
+    - parametrized
+- How to invoke jdb with mvn (with source files)
+    - How to create breakpoints
+    - Important jdb commands
+- How to execute a single test case via mvn
+- Handling nulls in Java
+*/
