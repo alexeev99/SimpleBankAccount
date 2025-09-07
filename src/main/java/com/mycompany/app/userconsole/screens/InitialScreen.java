984e7212baaedb11ec0d0f7d79b1f559f8f3cf12
Alex Alexeev <21alexeev99@gmail.com>
2025-09-07 23:37:02 +0200

    BANK-6: Implemented javadocs for InitialScreen class

--- src/main/java/com/mycompany/app/userconsole/screens/InitialScreen.java
+++ src/main/java/com/mycompany/app/userconsole/screens/InitialScreen.java
@@ -2,7 +2,30 @@ package com.mycompany.app.userconsole.screens;
 
 import com.mycompany.app.userconsole.UserConsoleScreen;
 
+/**
+ * Represents the initial user console screen of the SimpleBankApp. <br>
+ * <br>
+ * This screen welcomes the user to the application and presents a menu of available operations. It
+ * provides the user with instructions on how to proceed by typing the corresponding number or
+ * character for the desired operation.
+ *
+ * <p>The available options are:
+ *
+ * <ul>
+ *   <li><b>1</b> - Create a new Bank Account
+ *   <li><b>2</b> - Deposit money into an existing Bank Account
+ *   <li><b>c</b> - Terminate the application
+ * </ul>
+ *
+ * <p>Typical usage example:
+ *
+ * <pre>{@code
+ * System.out.print(InitialScreen.getText());
+ * String userInput = scanner.nextLine();
+ * }</pre>
+ */
 public class InitialScreen {
+
   private static final String TEXT =
       """
       Welcome to the SimpleBankApp!
@@ -19,8 +42,15 @@ public class InitialScreen {
 
       Your input: \
       """;
+
   private static UserConsoleScreen initialScreen = new UserConsoleScreen(TEXT);
 
+  /**
+   * Returns the initial screen text including the menu of available operations and instructions for
+   * the user.
+   *
+   * @return the initial screen text as a string
+   */
   public static String getText() {
     return InitialScreen.initialScreen.toString();
   }
