2d9c41ff072e703aab897be03a8239c779faee97
Alex Alexeev <21alexeev99@gmail.com>
2025-09-07 23:29:48 +0200

    BANK-6: Implemented javadocs for SuccessfulCreationScreen.java

--- src/main/java/com/mycompany/app/userconsole/screens/creationscreens/SuccessfulCreationScreen.java
+++ src/main/java/com/mycompany/app/userconsole/screens/creationscreens/SuccessfulCreationScreen.java
@@ -2,7 +2,23 @@ package com.mycompany.app.userconsole.screens.creationscreens;
 
 import com.mycompany.app.userconsole.UserConsoleScreen;
 
+/**
+ * Represents the console screen that is shown after a bank account has been successfully created.
+ * <br>
+ * <br>
+ * This screen displays a formatted confirmation message including the customer's first and last
+ * name as well as the newly assigned bank ID. The screen content can be customized by formatting
+ * the predefined success template with actual customer and bank data.
+ *
+ * <p>Typical usage example:
+ *
+ * <pre>{@code
+ * SuccessfulCreationScreen.setSuccessfulTextFormatting("Alice", "Smith", "123-456-789");
+ * System.out.print(SuccessfulCreationScreen.getSuccessfulText());
+ * }</pre>
+ */
 public class SuccessfulCreationScreen {
+
   private static final String SUCCESSFUL_TEXT =
       """
       Successfully created a new Customer: '%s %s'
@@ -11,12 +27,25 @@ public class SuccessfulCreationScreen {
 
   private static UserConsoleScreen successfulText = new UserConsoleScreen(SUCCESSFUL_TEXT);
 
+  /**
+   * Formats the success message using the provided customer details and updates the screen
+   * representation accordingly.
+   *
+   * @param firstName the first name of the created customer
+   * @param lastName the last name of the created customer
+   * @param bankId the unique bank account identifier assigned to the customer
+   */
   public static void setSuccessfulTextFormatting(String firstName, String lastName, String bankId) {
     String formattedText =
         String.format(SuccessfulCreationScreen.SUCCESSFUL_TEXT, firstName, lastName, bankId);
     SuccessfulCreationScreen.successfulText = new UserConsoleScreen(formattedText);
   }
 
+  /**
+   * Returns the currently formatted success message as plain text.
+   *
+   * @return the formatted success message string
+   */
   public static String getSuccessfulText() {
     return SuccessfulCreationScreen.successfulText.toString();
   }
